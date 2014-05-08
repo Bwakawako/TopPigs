db7d92c58c6c4b97f9d6f94a553330b81bcad2b3
Bwakawako <nicool.dt@gmail.com>
2014-05-08 17:59:34 +0200

    Script v1.0

---
+++ TopPig.ps1
@@ -0,0 +1,23 @@
+﻿param ([string]$Path=(pwd), [int]$HeadNumber=10)
+
+Function GetDirectorySize([string]$Path)
+{
+    $result = Get-ChildItem -Force -Path $Path | Measure-Object Length -Sum -ErrorAction Ignore
+    if ($result -eq $null)
+    {
+        return 0
+    }
+    return $result.Sum
+}
+
+GetDirectorySize "C:\Users\nviet\Desktop\batch\BAS"
+
+$Directories = @{}
+
+dir -Path $Path -Recurse -Force |? { $_.PSIsContainer -eq $True } |% { $Directories.Add($_.FullName, (GetDirectorySize $_.FullName) / 1000000)  }
+
+"Taille en Mo"
+
+#TODO Script block : { $_.Value }. COmment ca marche?
+$Directories.GetEnumerator() | Sort-Object { $_.Value } -Descending | Select-Object -First $HeadNumber | Format-Table -Property Key, Value -AutoSize
+#C:\Users\nviet\Desktop\batch\TopPig.ps1
